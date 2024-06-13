93b2c6b11e0432ffb187f5327e72f9c32b68bf0e
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-13 16:47:16 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BlitzIsKing/UniversalFarm/main/Loader/Regular"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxsta72/ProjectAuto/main/AutoRobV4Release"))()
 wait(60)
 game:GetService('TeleportService'):Teleport(8990888133, game:GetService('Players').LocalPlayer)
