cf98207001e21232c9e76c93f8a654783d455a5b
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-13 16:33:29 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlitzIsKing/UniversalFarm/main/Loader/Regular"))()
-task.wait(math.random(60,120))
-game:GetService('TeleportService'):Teleport(8990888133, game.Players.LocalPlayer)
+wait(60)
+game:GetService('TeleportService'):Teleport(8990888133, game:GetService('Players').LocalPlayer)
