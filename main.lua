fa85bd177dff25dad77113c079ff8526382e42c6
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-17 21:31:26 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/173862044fe31f1dcd4df0703280ff88.lua"))()
-wait(360)
+wait(600)
 game:GetService('TeleportService'):Teleport(8990888133, game:GetService('Players').LocalPlayer)
