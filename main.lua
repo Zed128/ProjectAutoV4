dcc37f78df3d5b282672638fee535c9614e604a9
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-20 17:20:10 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/BlitzIsKing/UniversalFarm/main/Loader/Regular'))()
 wait(200)
+while wait(300)
 game.Players.LocalPlayer.Character.Health = 0
-wait(400)
-game:GetService('TeleportService'):Teleport(8990888133, game:GetService('Players').LocalPlayer)
+end
