8194cac13620e2bee835abbab397ada627bea326
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-20 17:24:20 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/BlitzIsKing/UniversalFarm/main/Loader/Regular'))()
-wait(200)
 while wait(300)
 game.Players.LocalPlayer.Character.Health = 0
 end
