7834c4767933d698d507bca66b97d84a88201587
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-20 17:27:35 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BlitzIsKing/UniversalFarm/main/Loader/Regular'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxsta72/ProjectAuto/main/AutoRobV4Release"))()
 while wait(300)
 game.Players.LocalPlayer.Character.Health = 0
 end
