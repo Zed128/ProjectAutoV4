9def5bba6ab5d020adec5b16be64719a35052c62
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-20 17:34:51 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxsta72/ProjectAuto/main/AutoRobV4Release"))()
-while wait(300)
-    game.Players.LocalPlayer.Character.Health = 0
-end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BlitzIsKing/UniversalFarm/main/Loader/Regular'))()
+wait(200)
+game.Players.LocalPlayer.Character.Health = 0
+wait(400)
+game:GetService('TeleportService'):Teleport(8990888133, game:GetService('Players').LocalPlayer)
