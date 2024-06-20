d5fe8a6b656f77f78fa56b8896aa10bfbf0e0df0
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-20 18:09:02 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ spawn(function()
 end)
 spawn(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxsta72/ProjectAuto/main/AutoRobV4Release"))()
+end)
+spawn(function()
 while task.wait(30,120)
     game.Players.LocalPlayer.Character.Humanoid.Health = 0
 end
