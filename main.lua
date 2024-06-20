6d8d9ea789da7fa415f93b9e2620ca5dca45dd68
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-20 18:20:25 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ spawn(function()
     game:Shutdown()
 end)
 spawn(function()
-    while task.wait(30,120)
+    while task.wait(30,120) do
         game.Players.LocalPlayer.Character.Humanoid.Health = 0
     end
 end)
