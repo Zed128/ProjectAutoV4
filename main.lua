d76141c52de1c16d8b7b0a64a3aa384233ebb95e
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-20 17:27:52 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxsta72/ProjectAuto/main/AutoRobV4Release"))()
 while wait(300)
-game.Players.LocalPlayer.Character.Health = 0
+    game.Players.LocalPlayer.Character.Health = 0
 end
