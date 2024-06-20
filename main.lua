ab7a61e88cdd1c8df80c1e6da8a4bc302718c474
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-20 17:47:44 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxsta72/ProjectAuto/main/AutoRobV4Release"))()
-wait(200)
-game.Players.LocalPlayer.Character.Health = 0
-wait(400)
-game:GetService('TeleportService'):Teleport(8990888133, game:GetService('Players').LocalPlayer)
+while task.wait(30,120)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
