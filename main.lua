3e29b9b98496b490c41fff0fd7f331136b51b374
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-21 20:27:08 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,6 +7,10 @@ spawn(function()
     game:Shutdown()
 end)
 spawn(function()
+    task.wait(60)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+end)
+spawn(function()
     while task.wait(math.random(20,40)) do
         game.Players.LocalPlayer.Character.Humanoid.Health = 0
     end
