edf4d55481b4fd6d3fdee165e7828b702a18f504
Zed128 <118735074+Zed128@users.noreply.github.com>
2024-06-22 19:07:27 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-
+local lastRootPos = Vector3.new(0,0,0)
 spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxsta72/ProjectAuto/main/AutoRobV4Release"))()
     --loadstring(game:HttpGet('https://raw.githubusercontent.com/BlitzIsKing/UniversalFarm/main/Loader/Regular'))()
@@ -9,7 +9,12 @@ spawn(function()
 end)
 spawn(function()
     while task.wait(10) do
-        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Position = lastRootPos
+    end
+end)
+spawn(function()
+    while task.wait(3) do
+        lastRootPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
     end
 end)
 spawn(function()
